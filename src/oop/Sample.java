4e3230e2b39dff2f1bada4dfac5426d7ed88536a
hikeong <s25048@gsm.hs.kr>
2026-03-26 20:09:51 +0900

    oop

---
+++ src/oop/Sample.java
@@ -0,0 +1,20 @@
+package oop;
+
+public class Sample {
+    public static void main(String[] args) {
+        System.out.println(Calculator.add(3));
+        System.out.println(Calculator.add(4));
+    }
+}
+
+class Calculator {
+
+    static int result = 0;
+
+    static int add (int num) {
+        result += num;
+        return result;
+
+    }
+}
+
