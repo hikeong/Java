a96103c2371632ec043c18cbfdfeb5f6878d0daf
hikeong <s25048@gsm.hs.kr>
2026-03-30 23:22:41 +0900

    list

---
+++ src/oop/Sample3.java
@@ -0,0 +1,12 @@
+package oop;
+
+import java.util.ArrayList;
+
+public class Sample3 {
+    public static void main(String[] args) {
+        ArrayList pitches = new ArrayList();
+        pitches.add("138");
+        pitches.add("129");
+        pitches.add("142");
+    }
+}
