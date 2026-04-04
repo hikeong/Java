766e9febcf4aa36fadb594a751fa350fedd29cc7
hikeong <s25048@gsm.hs.kr>
2026-04-04 10:00:00 +0900

    static

---
+++ src/oop/Sample10.java
@@ -0,0 +1,22 @@
+package oop;
+
+class Singleton {
+    private static Singleton one;
+    private Singleton() {
+    }
+
+    public static Singleton getInstance() {
+        if(one==null) {
+            one = new Singleton();
+        }
+        return one;
+    }
+}
+
+public class Sample10 {
+    public static void main(String[] args) {
+        Singleton singleton1 = Singleton.getInstance();
+        Singleton singleton2 = Singleton.getInstance();
+        System.out.println(singleton1 == singleton2);  // true 출력
+    }
+}
