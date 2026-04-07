fb6a2a2e10da4510b28274b5650794c5b6d83064
hikeong <s25048@gsm.hs.kr>
2026-04-07 14:37:15 +0900

    Thread

---
+++ src/oop/Sample6.java
@@ -0,0 +1,12 @@
+package oop;
+
+public class Sample6 extends Thread {
+    public void run() {  // Thread 를 상속하면 run 메서드를 구현해야 한다.
+        System.out.println("thread run.");
+    }
+
+    public static void main(String[] args) {
+        Sample sample = new Sample();
+        sample.start();  // start()로 스레드를 실행한다.
+    }
+}
