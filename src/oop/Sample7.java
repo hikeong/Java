86e85945e473f17fd7f6914c807baaecfca2e856
hikeong <s25048@gsm.hs.kr>
2026-04-09 19:35:11 +0900

    콘솔 입출력

---
+++ src/oop/Sample7.java
@@ -0,0 +1,15 @@
+package oop;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Sample7 {
+    public static void main(String[] args) throws IOException {
+        InputStream in = System.in;
+
+        int a;
+        a = in.read();
+
+        System.out.println(a);
+    }
+}
